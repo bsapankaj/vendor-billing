fe7d02d934a97bf167e395449c0f0814d72c6586
bsapankaj <bsapankajdk@gmail.com>
2025-07-27 12:16:48 +0530

    add & update

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
-  database: 'testdb'
+  database: 'vendor_billing'
 });
 
 app.get('/users', (req, res) => {
